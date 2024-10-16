0b2a1dd34ea7ba1d537ef5b325914b770fea5495
Nguyen Van Tam <tamnvhustcc@gmail.com>
2024-10-16 10:31:14 +0700

    update

--- next.config.js
+++ next.config.js
@@ -1,8 +1,9 @@
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   rewrites: async () => {
     return [
       {
-        source: '/:path*',
+        source: '/api/:path*',
         destination:
           process.env.NODE_ENV === 'development'
             ? 'http://127.0.0.1:5328/:path*'
