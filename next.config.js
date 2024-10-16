924aecbe0f0fe538afa338862e12fc152593ff74
Nguyen Van Tam <tamnvhustcc@gmail.com>
2024-10-16 09:54:06 +0700

    update

--- next.config.js
+++ next.config.js
@@ -1,10 +1,15 @@
-module.exports = {
-  async rewrites() {
+const nextConfig = {
+  rewrites: async () => {
     return [
       {
         source: '/:path*',
-        destination: 'http://127.0.0.1:5328/:path*', // Proxy to Backend
+        destination:
+          process.env.NODE_ENV === 'development'
+            ? 'http://127.0.0.1:5328/:path*'
+            : '/',
       },
     ]
   },
 }
+
+module.exports = nextConfig
